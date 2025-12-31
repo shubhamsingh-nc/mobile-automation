e1dfd9b2b223fd33c6e56b07680f9b9659229192
nym-android-dev <zakiuddin.khan@nymcard.com>
2025-12-31 20:33:26 +0500

    feat: ata masaar forgot password tests flow in progress

---
+++ ata/.maestro/elements/forgot-passcode.js
@@ -0,0 +1,24 @@
+// Forgot Passcode Flow Elements
+// Contains all element IDs and selectors used in forgot passcode flows
+
+output.forgotPasscode = {
+  goToForgotPasscode: 'forgot_your_passcode',
+
+  mobileNumberVerified: 'mobile_number_verified',
+  emailAddressVerified: 'email_address_verified',
+
+  verifyEmailAddressButton: 'verify_email_address_button',
+
+  otpVerifyMobileNumberTitle: 'otp_verify_mobile_number_title',
+  otpVerifyEmailAddressTitle: 'otp_verify_email_title',
+
+  setPassCodeFlowButton: 'set_passcode_flow_button',
+
+  createPassCodeTitle: 'create_passcode_title',
+  confirmPassCodeTitle: 'confirm_passcode_title',
+  confirmCreatePasscodeButton: 'confirm_create_passcode_button',
+
+  passcodeUpdatedSuccessfully: 'passcode_updated_successfully'
+
+
+}
