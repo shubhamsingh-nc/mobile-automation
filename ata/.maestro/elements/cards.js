21bf1f73f04f4257b41f83a627463218124555ca
zakiuddinkhan-nc <zakiuddin.khan@nymcard.com>
2026-01-06 00:44:56 +0500

    feat: card freeze unfreeze and show and hide details tests added

---
+++ ata/.maestro/elements/cards.js
@@ -0,0 +1,29 @@
+// Cards related Page Elements
+// Contains all element IDs and selectors used in cards flows
+
+output.cards = {
+    homeCardTile: 'my_card_tile',
+    cardStateEmpty: 'card_details_empty',
+    cardStatePresent: 'card_details_present',
+    cardChipAndNFC: 'card_chip_and_nfc',
+
+    showDetailsButton: 'show_details',
+    hideDetailsButton: 'hide_details',
+    freezeCardButton: 'freeze_card',
+    unfreezeCardButton: 'unfreeze_card',
+    cardControlsButton: 'card_controls',
+    limitsButton: 'limits',
+    managePinButton: 'manage_pin',
+    replaceCardButton: 'replace_card',
+
+    copyCard: 'copy_card_number',
+    cardNumberVisible: 'card_number_visible',
+    cardNumberHidden: 'card_number_hidden',
+    cardExpiryVisible: 'card_expiry_visible',
+    cardExpiryHidden: 'card_expiry_hidden',
+    cvvVisible: 'cvv_visible',
+    cvvHidden: 'cvv_hidden',
+
+    cardFrozenBackground: 'frozen_card_background',
+    cardFrozenLabel: 'frozen_card_label'
+}
